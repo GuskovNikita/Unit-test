0bf1cfc428d969752b705ed4cf504045d32cee7c
GuskovNikita <113462036+GuskovNikita@users.noreply.github.com>
2022-10-03 14:44:46 +0300

    Delete Unit-test directory

--- Unit-test/src/Unit/Main.java
+++
@@ -1,9 +0,0 @@
-package Unit;
-
-public class Main {
-    public static void main(String[] args){
-        Numbers numbers = new Numbers();
-        int sum = numbers.sum(5);
-        System.out.println("Sum from 0 till 5 = " + sum);
-    }
-}
